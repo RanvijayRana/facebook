516d888b541830c8dc3c7dcb3ff3fa2bbd02e17b
mr ranvijay <ranvijay.cse@outlook.com>
2018-08-13 21:59:26 +0530

    set and retrieve updated

--- index.ts
+++ index.ts
@@ -154,7 +154,7 @@ class Person{
         return this.currentCity;
     }
 
-    getCityLived= (): string[] =>{
+    getPlaceLived= (): string[] =>{
         return this.placeLived;
     }
 
@@ -363,7 +363,145 @@ let placeVistited: string[] = ["Vaishali","Muzaffarpur","Kota","Durgapur","Chenn
 let ranvijay = new Person("Senior Systems Engineer","Infosys",designation,workPlace,"Bangalore","Vaishali",
                             placeVistited,9751862357);
 
+// all the information is tidious to pass through constructor so we use set/update method
+
+//set data for work and education section
+let skill1: string = "HTML";
+let skill2: string = "CSS";
+ranvijay.updateProfessionalSkill(skill1);
+ranvijay.updateProfessionalSkill(skill2);
+
+let college: string = "National Institute of Technology";
+let collegeYear: number = 2014;
+let degree: string = "B.Tech";
+let location1: string = "Durgapur";
+ranvijay.updateCollege(college,collegeYear,degree,location1);
+
+let school: string = "Prestine Children High School";
+let schoolYear: number = 2007;
+let schoolLocation1: string = "Muzaffarpur";
+ranvijay.updateSchool(school,schoolYear,schoolLocation1);
+
+//place lived has been update through constructor; in-time if we need to update call updateCurrentCity and hometown
+// method
+
+//contact information
 let contactDetails: number = 8778243965;
 ranvijay.setSecondaryContact(contactDetails);
-console.log(ranvijay);
 
+let address: string = "Concorde Silicon Valley";
+let cityTown: string = "Bangalore";
+let zip: number = 560100;
+let neighborhood: string ="Electronic City";
+ranvijay.updateAddress(address, cityTown, zip, neighborhood);
+
+let email: string = "ranvijay.144@gmail.com";
+ranvijay.updateEmail(email);
+
+let fbEmail: string = "ranvijay.rana@facebook.com";
+ranvijay.updateFbEmail(fbEmail);
+
+let birthDate: number = 20;
+let birthMonth: string = "Dec";
+let birthYear: number = 1992;
+ranvijay.updateBirth(birthDate, birthMonth, birthYear);
+let currentYear: number = 2018;
+ranvijay.updateAge(currentYear);
+
+let socialLinkAdd: string = "ranvijay.144@skype.com";
+let socialLinkSite: string = "Skype";
+ranvijay.updateSocialLink(socialLinkAdd, socialLinkSite);
+
+let gender: string = "Male";
+let interestedIn: string = "Female";
+let language1: string = "Hindi";
+let language2: string = "English";
+let religiousView: string = "Hinduism";
+let politicalView: string = "Democratic";
+ranvijay.updateGender(gender);
+ranvijay.updateInterestedIn(interestedIn);
+ranvijay.updateLanguage(language1);
+ranvijay.updateLanguage(language2);
+ranvijay.updateReligiousView(religiousView);
+ranvijay.updatePoliticalView(politicalView);
+
+let relationshipStatus: string = "Single";
+let familyMember1: string = "Sonu";
+let familyMember2: string = "Radha";
+let familyRelation1: string = "Brother";
+let familyRelation2: string = "Sister";
+ranvijay.updateRelationship(relationshipStatus);
+ranvijay.updatefamily(familyMember1, familyMember1);
+ranvijay.updatefamily(familyMember2, familyMember2);
+
+let eventName1: string = "BirthYear";
+let eventYear1: number = 1992;
+let eventName2: string = "Sonu Birthday";
+let eventYear2: number = 1995;
+let eventName3: string = "Facebook joined";
+let eventYear3: number = 2014;
+ranvijay.updateEvent(eventName1, eventYear1);
+ranvijay.updateEvent(eventName2, eventYear2);
+ranvijay.updateEvent(eventName3, eventYear3);
+
+let description: string = "helpful";
+let nickName1: string = "rana";
+let nickName2: string = "ranvi";
+let quotes: string = "Never do at expense of other!!!";
+ranvijay.updateDescription(description);
+ranvijay.updatenickname(nickName1);
+ranvijay.updatenickname(nickName2);
+ranvijay.updateFavQuotes(quotes);
+
+//retrieve data
+console.log(ranvijay.getCurrentDesignation);
+console.log(ranvijay.getCurrentWorkPlace);
+console.log(ranvijay.getDesignation);
+console.log(ranvijay.getWorkPlace);
+
+console.log(ranvijay.getProfessionalSkill);
+console.log(ranvijay.getCollege);
+console.log(ranvijay.getCollegeYear);
+console.log(ranvijay.getDegree);
+console.log(ranvijay.getCollegeLocation);
+
+console.log(ranvijay.getSchool);
+console.log(ranvijay.getSchoolYear);
+console.log(ranvijay.getSchoolLocation);
+
+console.log(ranvijay.getCurrentCity);
+console.log(ranvijay.getPlaceLived);
+console.log(ranvijay.getHomeTown);
+
+console.log(ranvijay.getAddress);
+console.log(ranvijay.getCityTown);
+console.log(ranvijay.getZip);
+console.log(ranvijay.getNeighborhood);
+
+console.log(ranvijay.getEmail);
+console.log(ranvijay.getFbEmail);
+
+console.log(ranvijay.getSocialLinkAdd);
+console.log(ranvijay.getSocialLinkSite);
+
+console.log(ranvijay.getBirthDate);
+console.log(ranvijay.getBirthMonth);
+console.log(ranvijay.getBirthYear);
+console.log(ranvijay.getAge);
+
+console.log(ranvijay.getGender);
+console.log(ranvijay.getInterestedIn);
+console.log(ranvijay.getLanguage);
+console.log(ranvijay.getReligiousView);
+console.log(ranvijay.getPoliticalView);
+
+console.log(ranvijay.getRelationshipStatus);
+console.log(ranvijay.getFamilyMember);
+console.log(ranvijay.getFamilyRelation);
+
+console.log(ranvijay.getDescription);
+console.log(ranvijay.getnickName);
+console.log(ranvijay.getFavQuotes);
+
+console.log(ranvijay.getEventName);
+console.log(ranvijay.getEventYear);
